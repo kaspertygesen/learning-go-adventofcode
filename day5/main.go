bfa23ef5edf6dc595a2a44c445ab6c43fd6698c3
Kasper Hareskov Tygesen <kastygesen@gmail.com>
2024-12-08 22:49:16 +0100

    Day 5 - Part 2 completed

--- day5/main.go
+++ day5/main.go
@@ -18,24 +18,38 @@ func main() {
 
 	fmt.Printf("Part 1: %d\n", part1)
 
-	// invalidUpdates := invalidUpdates(rules, updates)
+	invalidUpdates := invalidUpdates(rules, updates)
 
-	// fmt.Print(invalidUpdates)
+	correctInvalidUpdates := correctInvalidUpdates(rules, invalidUpdates)
 
-	part2 := 0
+	part2 := sumMiddle(correctInvalidUpdates)
 
 	fmt.Printf("Part 2: %d\n", part2)
 }
 
-// func correctInvalidUpdates(updates [][]int) {
-// 	for _, u := range updates {
-// 		slices.SortFunc(u, func(a, b int) int {
-// 			return -1
-// 		})
-// 	}
-// }
+func correctInvalidUpdates(rules *[][]int, updates [][]int) [][]int {
+	var correctedUpdates [][]int
 
-func invalidUpdates(rules [][]int, updates [][]int) [][]int {
+	for _, u := range updates {
+		slices.SortFunc(u, func(a, b int) int {
+			for _, r := range *rules {
+				if r[0] == a && r[1] == b {
+					return -1
+				} else if r[1] == a && r[0] == b {
+					return 1
+				}
+			}
+
+			return 0
+		})
+
+		correctedUpdates = append(correctedUpdates, u)
+	}
+
+	return correctedUpdates
+}
+
+func invalidUpdates(rules *[][]int, updates [][]int) [][]int {
 	var invalidUpdates [][]int
 
 	for _, u := range updates {
@@ -43,7 +57,7 @@ func invalidUpdates(rules [][]int, updates [][]int) [][]int {
 
 		for uIndex, n := range u {
 
-			for _, r := range rules {
+			for _, r := range *rules {
 				if r[0] == n {
 					if ruIndex := slices.Index(u, r[1]); ruIndex > -1 {
 						if uIndex > ruIndex {
@@ -81,7 +95,7 @@ func sumMiddle(updates [][]int) int {
 	return sum
 }
 
-func validUpdates(rules [][]int, updates [][]int) [][]int {
+func validUpdates(rules *[][]int, updates [][]int) [][]int {
 	var validUpdates [][]int
 
 	for _, u := range updates {
@@ -89,7 +103,7 @@ func validUpdates(rules [][]int, updates [][]int) [][]int {
 
 		for uIndex, n := range u {
 
-			for _, r := range rules {
+			for _, r := range *rules {
 				if r[0] == n {
 					if ruIndex := slices.Index(u, r[1]); ruIndex > -1 {
 						if uIndex > ruIndex {
@@ -120,7 +134,7 @@ func validUpdates(rules [][]int, updates [][]int) [][]int {
 	return validUpdates
 }
 
-func parseInputFile(path string) (rules, updates [][]int) {
+func parseInputFile(path string) (rules *[][]int, updates [][]int) {
 	file, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -128,6 +142,8 @@ func parseInputFile(path string) (rules, updates [][]int) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+
+	var rulesArr [][]int
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -148,7 +164,7 @@ func parseInputFile(path string) (rules, updates [][]int) {
 			rule = append(rule, number)
 		}
 
-		rules = append(rules, rule)
+		rulesArr = append(rulesArr, rule)
 	}
 
 	for scanner.Scan() {
@@ -167,7 +183,7 @@ func parseInputFile(path string) (rules, updates [][]int) {
 		updates = append(updates, update)
 	}
 
-	return
+	return &rulesArr, updates
 }
 
 func parseInt(str string) (i int, err error) {
--- day5/main_test.go
+++ day5/main_test.go
@@ -6,6 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSumMiddleCorrected(t *testing.T) {
+	rules, updates := parseInputFile("test_input.txt")
+	correctedUpdates := correctInvalidUpdates(rules, invalidUpdates(rules, updates))
+	sum := sumMiddle(correctedUpdates)
+
+	assert.Equal(t, 123, sum)
+}
+
+func TestCorrectInvalidUpdates(t *testing.T) {
+	rules, updates := parseInputFile("test_input.txt")
+	correctedUpdates := correctInvalidUpdates(rules, invalidUpdates(rules, updates))
+
+	assert.Equal(t, []int{97, 75, 47, 61, 53}, correctedUpdates[0])
+	assert.Equal(t, []int{61, 29, 13}, correctedUpdates[1])
+	assert.Equal(t, []int{97, 75, 47, 29, 13}, correctedUpdates[2])
+	assert.Len(t, correctedUpdates, 3)
+}
+
 func TestInvalidUpdates(t *testing.T) {
 	updates := invalidUpdates(parseInputFile("test_input.txt"))
 
@@ -33,8 +51,8 @@ func TestValidUpdates(t *testing.T) {
 func TestInputParser(t *testing.T) {
 	rules, updates := parseInputFile("test_input.txt")
 
-	assert.Equal(t, []int{47, 53}, rules[0])
-	assert.Equal(t, []int{53, 13}, rules[len(rules)-1])
+	assert.Equal(t, []int{47, 53}, (*rules)[0])
+	assert.Equal(t, []int{53, 13}, (*rules)[len(*rules)-1])
 
 	assert.Equal(t, []int{75, 47, 61, 53, 29}, updates[0])
 	assert.Equal(t, []int{97, 13, 75, 29, 47}, updates[len(updates)-1])
