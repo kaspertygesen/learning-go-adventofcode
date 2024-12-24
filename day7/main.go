8cb76fa4116ba0502844d8b2aea89392146e4638
Kasper Hareskov Tygesen <kastygesen@gmail.com>
2024-12-24 16:02:02 +0100

    Day 7 - Part 1 completed

---
+++ day7/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	equations := parseInputFile("input.txt")
+
+	validEquations := evaluateEquations(equations)
+
+	part1 := sumResults(validEquations)
+
+	fmt.Printf("Part 1: %d\n", part1)
+	fmt.Printf("Part 2: %d\n", 0)
+}
+
+func sumResults(equations *[]equation) int64 {
+	var sum int64 = 0
+
+	for _, e := range *equations {
+		sum += e.result
+	}
+
+	return sum
+}
+
+func evaluateEquations(equations *[]equation) *[]equation {
+	var validEquations []equation
+
+	maxLength := 0
+	for _, e := range *equations {
+		if l := len(e.numbers); maxLength < l {
+			maxLength = l
+		}
+	}
+
+	var combinations []string
+	generateCombinations("", maxLength-1, "+*", &combinations)
+
+	for _, e := range *equations {
+		if !slices.ContainsFunc(e.numbers, func(n int64) bool { return n < 2 }) {
+			var sum int64 = 0
+			var product int64 = 1
+
+			for _, n := range e.numbers {
+				sum += n
+				product *= n
+			}
+
+			if e.result < sum || e.result > product {
+				//fmt.Print(e)
+				//fmt.Println("Impossible")
+				continue
+			} else if e.result == sum || e.result == product {
+				//fmt.Print(e)
+				//fmt.Println("Valid")
+				validEquations = append(validEquations, e)
+				continue
+			}
+		}
+
+		for _, c := range combinations {
+			operators := c[:len(e.numbers)-1]
+
+			result := e.numbers[0]
+
+			for i, n := range e.numbers[1:] {
+				switch operators[i] {
+				case '+':
+					result += n
+				case '*':
+					result *= n
+				}
+			}
+
+			if result == e.result {
+				validEquations = append(validEquations, e)
+				break
+			}
+		}
+	}
+
+	return &validEquations
+}
+
+func generateCombinations(s string, d int, operators string, combinations *[]string) {
+	for _, o := range operators {
+		if d != 0 {
+			generateCombinations(s+string(o), d-1, operators, combinations)
+		}
+	}
+
+	if d == 0 {
+		*combinations = append(*combinations, s)
+	}
+}
+
+func parseInputFile(path string) *[]equation {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var equations []equation
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		parts := strings.Split(line, ": ")
+
+		numbersStr := strings.Fields(parts[1])
+
+		result, _ := parseInt64(parts[0])
+
+		var numbers []int64
+
+		for _, v := range numbersStr {
+			if vInt, err := parseInt64(v); err == nil {
+				numbers = append(numbers, vInt)
+			}
+		}
+
+		equations = append(equations, equation{result: int64(result), numbers: numbers})
+	}
+
+	return &equations
+}
+
+func parseInt64(str string) (i int64, err error) {
+	if v, err := strconv.ParseInt(str, 10, 64); err == nil {
+		return v, nil
+	}
+
+	return 0, err
+}
+
+type equation struct {
+	result int64
+
+	numbers []int64
+}
---
+++ day7/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEvaluateEquations(t *testing.T) {
+	equations := parseInputFile("test_input.txt")
+
+	validEquations := evaluateEquations(equations)
+
+	assert.Equal(t, 3, len(*validEquations))
+	assert.Equal(t, equation{result: 190, numbers: []int64{10, 19}}, (*validEquations)[0])
+	assert.Equal(t, equation{result: 3267, numbers: []int64{81, 40, 27}}, (*validEquations)[1])
+	assert.Equal(t, equation{result: 292, numbers: []int64{11, 6, 16, 20}}, (*validEquations)[2])
+}
+
+func TestGenerateCombinations(t *testing.T) {
+	var combinations []string
+
+	generateCombinations("", 3, "+*", &combinations)
+
+	assert.Equal(t, int(math.Pow(2, 3)), len(combinations))
+}
+
+func TestParseInput(t *testing.T) {
+	equations := parseInputFile("test_input.txt")
+
+	assert.Equal(t, equation{result: 190, numbers: []int64{10, 19}}, (*equations)[0])
+	assert.NotEqual(t, equation{result: 180, numbers: []int64{10, 19}}, (*equations)[0])
+	assert.NotEqual(t, equation{result: 190, numbers: []int64{11, 19}}, (*equations)[0])
+	assert.Equal(t, equation{result: 83, numbers: []int64{17, 5}}, (*equations)[2])
+	assert.Equal(t, equation{result: 292, numbers: []int64{11, 6, 16, 20}}, (*equations)[8])
+}
