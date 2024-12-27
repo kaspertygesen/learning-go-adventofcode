dac0efa9bd5ef224dff542e1ed6e8cb43e9ef029
Kasper Hareskov Tygesen <kastygesen@gmail.com>
2024-12-27 23:49:04 +0100

    Day 10 - Completed

---
+++ day10/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	terrainMap := parseInputFile("input.txt")
+
+	part1 := findPaths(terrainMap, true)
+
+	part2 := findPaths(terrainMap, false)
+
+	fmt.Printf("Part 1: %d\n", part1)
+
+	fmt.Printf("Part 2: %d\n", part2)
+}
+
+func findPaths(terrainMap *[][]int, distrinct bool) int {
+	count := 0
+
+	for y := 0; y < len(*terrainMap); y++ {
+		for x := 0; x < len((*terrainMap)[y]); x++ {
+			height := (*terrainMap)[y][x]
+
+			if height == 0 {
+				tops := evaluateTrailhead(terrainMap, x, y, distrinct)
+
+				fmt.Println(len(tops))
+
+				count += len(tops)
+			}
+		}
+	}
+
+	return count
+}
+
+func evaluateTrailhead(terrainMap *[][]int, x, y int, distrinct bool) []coordinate {
+	height := (*terrainMap)[y][x]
+
+	var tops []coordinate
+
+	if yu := y - 1; yu >= 0 {
+		tops = append(tops, evaluateNext(terrainMap, height, x, yu, distrinct)...)
+	}
+	if yd := y + 1; yd < len(*terrainMap) {
+		tops = append(tops, evaluateNext(terrainMap, height, x, yd, distrinct)...)
+	}
+
+	if xr := x + 1; xr < len(*terrainMap) {
+		tops = append(tops, evaluateNext(terrainMap, height, xr, y, distrinct)...)
+	}
+	if xl := x - 1; xl >= 0 {
+		tops = append(tops, evaluateNext(terrainMap, height, xl, y, distrinct)...)
+	}
+
+	if !distrinct {
+		return tops
+	}
+
+	var topMap map[coordinate]bool = make(map[coordinate]bool)
+	for _, t := range tops {
+		topMap[t] = true
+	}
+
+	var distinctTops []coordinate
+	for k := range topMap {
+		distinctTops = append(distinctTops, k)
+	}
+
+	return distinctTops
+}
+
+func evaluateNext(terrainMap *[][]int, height int, x, y int, distrinct bool) []coordinate {
+	if nextHeight := &(*terrainMap)[y][x]; *nextHeight == 9 && height == 8 {
+		return []coordinate{{x, y}}
+	} else if *nextHeight == height+1 {
+		return evaluateTrailhead(terrainMap, x, y, distrinct)
+	} else {
+		return []coordinate{}
+	}
+}
+
+func parseInputFile(path string) *[][]int {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var terrainMap [][]int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var numbers []int
+
+		for _, v := range line {
+			numbers = append(numbers, (int(v) - '0'))
+		}
+
+		terrainMap = append(terrainMap, numbers)
+	}
+
+	return &terrainMap
+}
+
+type coordinate struct {
+	x int
+	y int
+}
---
+++ day10/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindPaths(t *testing.T) {
+	input := parseInputFile("test_input.txt")
+
+	count := findPaths(input)
+
+	assert.Equal(t, 36, count)
+}
+
+func TestEvaluateTrailhead(t *testing.T) {
+	input := parseInputFile("test_input.txt")
+
+	tops := evaluateTrailhead(input, 2, 0)
+
+	assert.Contains(t, tops, coordinate{4, 3})
+	assert.Contains(t, tops, coordinate{0, 3})
+	assert.Contains(t, tops, coordinate{5, 4})
+	assert.Contains(t, tops, coordinate{4, 5})
+	assert.Contains(t, tops, coordinate{1, 0})
+	assert.Len(t, tops, 5)
+}
+
+func TestParseInput(t *testing.T) {
+	input := parseInputFile("test_input.txt")
+
+	assert.Equal(t, 8, (*input)[0][0])
+	assert.Equal(t, 1, (*input)[5][3])
+	assert.Equal(t, 2, (*input)[7][7])
+}
