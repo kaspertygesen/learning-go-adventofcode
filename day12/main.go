b75b427e44505f98646b262ee14df25147ffedfd
Kasper Hareskov Tygesen <kastygesen@gmail.com>
2025-01-05 12:11:01 +0100

    Day 12 - Part 2 completed

--- day12/main.go
+++ day12/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 )
 
 func main() {
@@ -12,10 +13,62 @@ func main() {
 	groups := group(input)
 
 	part1 := sum(groups)
-	part2 := 0
+	part2 := sum2(groups)
 
 	fmt.Printf("Part 1: %d\n", part1)
 	fmt.Printf("Part 2: %d\n", part2)
+}
+
+func sum2(groups [][]region) int {
+	sum := 0
+
+	for _, g := range groups {
+		edgeGroups := edgeGroups(g)
+
+		fences := fences(edgeGroups)
+
+		sum += fences * len(g)
+	}
+
+	return sum
+}
+
+func fences(edgeGroups map[edgeGroup][]int) int {
+	fences := 0
+	for _, edges := range edgeGroups {
+		slices.Sort(edges)
+
+		last := -1
+		for _, e := range edges {
+			if last == -1 {
+				fences++
+			} else if last+1 < e {
+				fences++
+			}
+
+			last = e
+		}
+	}
+
+	return fences
+}
+
+func edgeGroups(group []region) map[edgeGroup][]int {
+	edgeGroups := make(map[edgeGroup][]int)
+
+	for _, r := range group {
+		for _, e := range r.edges {
+			eg := edgeGroup{direction: e.direction, position: e.position}
+
+			//if _, ok := edgeGroups[eg]; ok {
+			edgeGroups[eg] = append(edgeGroups[eg], e.value)
+			//} else {
+			//	edgeGroups[eg] = []int{e.value}
+			//}
+		}
+	}
+
+	return edgeGroups
 }
 
 func sum(groups [][]region) int {
@@ -64,36 +117,65 @@ func search(garden *[][]region, p string, x, y int, group *[]region) bool {
 		r.visited = true
 		fences := 4
 
+		var edges []edge
+
 		if nx := x - 1; nx >= 0 {
 			if search(garden, p, nx, y, group) {
 				fences--
+			} else {
+				edges = append(edges, edge{direction: left, position: x, value: y})
 			}
+		} else {
+			edges = append(edges, edge{direction: left, position: x, value: y})
 		}
 
 		if ny := y - 1; ny >= 0 {
 			if search(garden, p, x, ny, group) {
 				fences--
+			} else {
+				edges = append(edges, edge{direction: up, position: y, value: x})
 			}
+		} else {
+			edges = append(edges, edge{direction: up, position: y, value: x})
 		}
 
 		if nx := x + 1; nx < len((*garden)[0]) {
 			if search(garden, p, nx, y, group) {
 				fences--
+			} else {
+				edges = append(edges, edge{direction: right, position: x, value: y})
 			}
+		} else {
+			edges = append(edges, edge{direction: right, position: x, value: y})
 		}
 
 		if ny := y + 1; ny < len((*garden)[0]) {
 			if search(garden, p, x, ny, group) {
 				fences--
+			} else {
+				edges = append(edges, edge{direction: down, position: y, value: x})
 			}
+		} else {
+			edges = append(edges, edge{direction: down, position: y, value: x})
 		}
 
 		r.fences = fences
+		r.edges = edges
+
 		*group = append(*group, *r)
 
 		return true
 	} else {
 		return false
+	}
+}
+
+func addCoordinates(garden *[][]region) {
+	for y := 0; y < len(*garden); y++ {
+		for x := 0; x < len((*garden)[y]); x++ {
+			(*garden)[y][x].x = x
+			(*garden)[y][x].y = y
+		}
 	}
 }
 
@@ -131,4 +213,27 @@ type region struct {
 	plant   string
 	visited bool
 	fences  int
+
+	x int
+	y int
+
+	edges []edge
+}
+
+const (
+	up    = iota
+	right = iota
+	down  = iota
+	left  = iota
+)
+
+type edge struct {
+	direction int
+	position  int
+	value     int
+}
+
+type edgeGroup struct {
+	direction int
+	position  int
 }
--- day12/main_test.go
+++ day12/main_test.go
@@ -1,10 +1,169 @@
 package main
 
 import (
+	"slices"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestSumE(t *testing.T) {
+	input := parseInputFile("test_input_e.txt")
+
+	addCoordinates(input)
+
+	groups := group(input)
+
+	sum := sum2(groups)
+
+	assert.Equal(t, 236, sum)
+}
+
+func TestFencesE(t *testing.T) {
+	input := parseInputFile("test_input_e.txt")
+
+	addCoordinates(input)
+
+	groups := group(input)
+
+	for _, g := range groups {
+		edgeGroups := edgeGroups(g)
+		fences := fences(edgeGroups)
+
+		if g[0].plant == "E" {
+			assert.Equal(t, 12, fences)
+		}
+		if g[0].plant == "X" && g[0].y == 1 {
+			assert.Equal(t, 4, fences)
+		}
+		if g[0].plant == "X" && g[0].y == 3 {
+			assert.Equal(t, 4, fences)
+		}
+	}
+}
+
+func TestEdgeGroupsE(t *testing.T) {
+	input := parseInputFile("test_input_e.txt")
+
+	addCoordinates(input)
+
+	groups := group(input)
+
+	assert.Len(t, groups, 3)
+
+	for _, g := range groups {
+		edgeGroups := edgeGroups(g)
+		if g[0].plant == "E" {
+			assert.Len(t, edgeGroups, 9)
+
+			// horizontal
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 0, position: 0}], []int{0, 1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 2, position: 0}], []int{1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 0, position: 2}], []int{1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 2, position: 2}], []int{1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 0, position: 4}], []int{1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 2, position: 4}], []int{0, 1, 2, 3, 4})
+
+			// vertical
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 3, position: 0}], []int{0, 1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 1, position: 0}], []int{1, 3})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 1, position: 4}], []int{0, 2, 4})
+		}
+		if g[0].plant == "X" && g[0].y == 1 {
+
+			assert.Len(t, edgeGroups, 4)
+
+			// horizontal
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 0, position: 1}], []int{1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 2, position: 1}], []int{1, 2, 3, 4})
+
+			// vertical
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 3, position: 1}], []int{1})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 1, position: 4}], []int{1})
+		}
+		if g[0].plant == "X" && g[0].y == 3 {
+			assert.Len(t, edgeGroups, 4)
+
+			// horizontal
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 0, position: 3}], []int{1, 2, 3, 4})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 2, position: 3}], []int{1, 2, 3, 4})
+
+			// vertical
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 3, position: 1}], []int{3})
+			assertEdgeGroup(t, edgeGroups[edgeGroup{direction: 1, position: 4}], []int{3})
+		}
+	}
+}
+
+func assertEdgeGroup(t *testing.T, actual []int, expected []int) {
+	slices.Sort(actual)
+
+	assert.Equal(t, actual, expected)
+}
+
+func TestSum2(t *testing.T) {
+	input := parseInputFile("test_input.txt")
+
+	addCoordinates(input)
+
+	groups := group(input)
+
+	sum := sum2(groups)
+
+	assert.Equal(t, 1206, sum)
+}
+
+// func TestGroupsEdges(t *testing.T) {
+// 	input := parseInputFile("test_input.txt")
+
+// 	addCoordinates(input)
+
+// 	groups := group(input)
+
+// 	assert.Len(t, groups, 11)
+// 	for _, g := range groups {
+// 		for _, r := range g {
+// 			if r.x == 0 && r.y == 0 {
+// 				assert.Len(t, r.edges, 2)
+// 				assert.Contains(t, r.edges, edge{direction: left, position: 0})
+// 				assert.Contains(t, r.edges, edge{direction: up, position: 0})
+// 			}
+// 			if r.x == 1 && r.y == 0 {
+// 				assert.Len(t, r.edges, 1)
+// 				assert.Contains(t, r.edges, edge{direction: up, position: 0})
+// 			}
+// 			if r.x == 2 && r.y == 0 {
+// 				assert.Len(t, r.edges, 1)
+// 				assert.Contains(t, r.edges, edge{direction: up, position: 0})
+// 			}
+// 			if r.x == 3 && r.y == 0 {
+// 				assert.Len(t, r.edges, 2)
+// 				assert.Contains(t, r.edges, edge{direction: up, position: 0})
+// 				assert.Contains(t, r.edges, edge{direction: right, position: 3})
+// 			}
+// 			if r.x == 3 && r.y == 1 {
+// 				assert.Len(t, r.edges, 1)
+// 				assert.Contains(t, r.edges, edge{direction: right, position: 3})
+// 			}
+// 			if r.x == 3 && r.y == 2 {
+// 				assert.Len(t, r.edges, 1)
+// 				assert.Contains(t, r.edges, edge{direction: down, position: 2})
+// 			}
+// 			if r.x == 4 && r.y == 2 {
+// 				assert.Len(t, r.edges, 3)
+// 				assert.Contains(t, r.edges, edge{direction: right, position: 4})
+// 				assert.Contains(t, r.edges, edge{direction: down, position: 2})
+// 				assert.Contains(t, r.edges, edge{direction: up, position: 2})
+// 			}
+// 			if r.x == 2 && r.y == 3 {
+// 				assert.Len(t, r.edges, 3)
+// 				assert.Contains(t, r.edges, edge{direction: right, position: 2})
+// 				assert.Contains(t, r.edges, edge{direction: down, position: 3})
+// 				assert.Contains(t, r.edges, edge{direction: left, position: 2})
+// 			}
+// 		}
+// 	}
+// }
 
 func TestSum(t *testing.T) {
 	input := parseInputFile("test_input.txt")
@@ -25,7 +184,7 @@ func TestGroups(t *testing.T) {
 func TestParseInput(t *testing.T) {
 	input := parseInputFile("test_input.txt")
 
-	assert.Equal(t, 'R', (*input)[0][0].plant)
-	assert.Equal(t, 'E', (*input)[9][9].plant)
+	assert.Equal(t, "R", (*input)[0][0].plant)
+	assert.Equal(t, "E", (*input)[9][9].plant)
 	assert.Len(t, *input, 10)
 }
